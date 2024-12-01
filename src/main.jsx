77bb12eb3cf25a95c2a0b5962793aea60693dfb8
Tevin Cheatham <tevincheatham@Tevins-MacBook-Pro.local>
2024-11-30 21:17:10 -0500

    reactDOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot, ReactDOM } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
 import {
